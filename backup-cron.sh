d6a2200049fea7f9683099ff506a8fa7818bcff7
deepfriednz <louistan3@gmail.com>
2017-10-14 16:43:16 +1300

    added logging and to source vars.config file.

--- backup-cron.sh
+++ backup-cron.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 state=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep state | awk '{print $2}')
+source "vars.config" #file that has pathToBackup variable
 
 if [[ $state == "dicharging" ]] 
 then
+	echo "On Battery, not running backups"
 	exit
 else
-	#path to backup script
+	echo "Running backups" 
+	exec $pathToBackup
 fi
 
