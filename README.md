6ae2d5fc0d5a99a16023a595faa1ccb634530f09
deepfriednz <louistan3@gmail.com>
2017-10-20 19:59:03 +1300

    added sample, added README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # backup-cron
 Contains the script for the b2backup script to run via cron
+
+Needs a variables config file called 
+
+vars.config 
+
+as per sample file
--- backup-cron.sh
+++ backup-cron.sh
@@ -3,12 +3,19 @@
 state=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep state | awk '{print $2}')
 source "vars.config" #file that has pathToBackup variable
 
+function runscript {
+	cd $pathToBackup
+        pwd
+	bash $backupScript
+}
+
 if [[ $state == "dicharging" ]] 
 then
 	echo "On Battery, not running backups"
 	exit
 else
 	echo "Running backups" 
-	exec $pathToBackup
+	runscript
+	
 fi
 
